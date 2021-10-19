d6ff3ba899718afafc5bdd0b1eb592190db881ab
kervis vasquez <kervisvasquez24@gmail.com>
2021-10-19 19:23:11 -0400

    u

--- lib/sliver-page.dart
+++ lib/sliver-page.dart
@@ -13,24 +13,40 @@ class SliverPage extends StatelessWidget {
 
 // header de nuestra app
 class _List_task extends StatelessWidget {
+  final items = [
+    _ListItem('El ', Color(0xffF08F66)),
+    _ListItem('Futuro', Color(0xffF2A38A)),
+    _ListItem('es hoy !!!', Color(0xffF7CDD5)),
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: 10,
-        itemBuilder: (BuildContext context, int index) => _ListItem());
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) => items[index]);
   }
 }
 
 class _ListItem extends StatelessWidget {
+  final String titulo;
+  final Color color;
+
+  const _ListItem(this.titulo, this.color);
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 130,
-      margin: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(30),
-      ),
-    );
+        height: 130,
+        padding: EdgeInsets.all(30),
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Text(
+          titulo,
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ));
   }
 }
